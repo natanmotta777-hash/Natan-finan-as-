d8e9fd5713f3ccb49f7bcee943bd5f0e35e11044
natanmotta777-hash <natanmotta777@gmail.com>
2026-09-11 22:00:43 -0500

    Adiciona funcionamento offline

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='natan-financas-v1';
+const APP=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
